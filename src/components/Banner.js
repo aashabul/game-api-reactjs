0a671833c9ad458df83010a408607032f1073df0
aashabul <aashabulimam@gmail.com>
2022-07-08 18:24:48 +0600

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,12 @@
 import React from "react";
-import video from "../assets/background-video.mp4";
+import video from "../assets/bg.mp4";
 
 const Banner = () => {
   return (
-    <div>
-      <video src={video} autoPlay loop muted width="100%" height="100%" />
+    <div style={{ width: "100%" }}>
+      <video autoPlay loop muted width="100%" height="100%">
+        <source src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/homepage/dota_montage_webm.webm" />
+      </video>
     </div>
   );
 };
